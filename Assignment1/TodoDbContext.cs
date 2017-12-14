d48496c97433761fc70029085e8b6f3f6931bd70
icvetnic <ivan.cvetnic@fer.hr>
2017-12-14 14:38:46 +0100

    Initial commit for Assignment1

---
+++ Assignment1/TodoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+
+namespace Assignment1
+{
+    public class TodoDbContext : DbContext
+    {
+        public IDbSet<TodoItem> TodoItems { get; set; }
+        public IDbSet<TodoItemLabel> TodoItemLabels { get; set; }
+
+        public TodoDbContext(string conectionString) : base(conectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TodoItem>().HasKey(td => td.Id);
+            modelBuilder.Entity<TodoItem>().Property(td => td.Text).IsRequired();
+            modelBuilder.Entity<TodoItem>().Property(td => td.IsCompleted).IsRequired();
+            modelBuilder.Entity<TodoItem>().Property(td => td.DateCompleted).IsOptional();
+            modelBuilder.Entity<TodoItem>().Property(td => td.DateCreated).IsRequired();
+            modelBuilder.Entity<TodoItem>().Property(td => td.UserId).IsRequired();
+            modelBuilder.Entity<TodoItem>().Property(td => td.DateDue).IsOptional();
+            modelBuilder.Entity<TodoItem>().HasMany(td => td.Labels).WithMany(tdl => tdl.LabelTodoItems);
+
+
+
+        }
+    }
+}
---
+++ Assignment1/TodoItem.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment1
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+
+        public bool IsCompleted => DateCompleted.HasValue;
+
+        public DateTime? DateCompleted { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// User id that owns this TodoItem
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// List of labels associated with TodoItem
+        /// </summary>  
+        public List<TodoItemLabel> Labels { get; set; }
+
+        /// <summary>
+        /// Date due. If null, no date was set by the user
+        /// </summary>
+        public DateTime? DateDue { get; set; }
+
+        public TodoItem()
+        {
+            // entity framework needs this one
+            // not for use :)
+        }
+
+        public TodoItem(string text)
+        {
+            // Generates new unique identifier
+            Id = Guid.NewGuid();
+
+            // DateTime .Now returns local time , it wont always be what you expect
+            //(depending where the server is).
+            // We want to use universal (UTC ) time which we can easily convert to
+            //local when needed.
+            // ( usually done in browser on the client side )
+            DateCreated = DateTime.UtcNow;
+
+            Text = text;
+        }
+
+        public bool MarkAsCompleted()
+        {
+            if (!IsCompleted)
+            {
+                DateCompleted = DateTime.Now;
+                return true;
+            }
+            return false;
+        }
+
+        public void ReplaceAllData(TodoItem todoItem)
+        {
+            Text = todoItem.Text;
+            DateCompleted = todoItem.DateCompleted;
+            DateCreated = todoItem.DateCreated;
+        }
+
+        protected bool Equals(TodoItem other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TodoItem) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
